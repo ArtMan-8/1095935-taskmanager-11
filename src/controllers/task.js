5f4cb083deafaba18dd5e8b48e387c6e0b2fd109
Art <ya.Artriches@ya.ru>
2020-04-23 20:23:50 +0300

    Создаёт контролер задачи

---
+++ src/controllers/task.js
@@ -0,0 +1,14 @@
+import {renderComponent, RenderPosition, replaceComponent} from "../utils/render";
+
+import TaskComponent from "../components/task";
+import TaskEditComponent from "../components/task-edit";
+
+export default class TaskControler {
+  constructor() {
+
+  }
+
+  render(task) {
+
+  }
+}
